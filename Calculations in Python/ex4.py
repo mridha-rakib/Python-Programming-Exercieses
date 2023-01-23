819b894e7a8cf296735af399968b0ada2e74cd71
RAKIB <rakibmahmudmridha@gmail.com>
2023-01-24 00:15:45 +0600

    new files added

---
+++ Calculations in Python/ex4.py
@@ -0,0 +1,5 @@
+a1 = 14
+a10 = 50
+n = 10
+s10 = ((a1 + a10) / 2) * n
+print(f'The sum of the first 10 elements in a sequence: {s10}')
---
+++ Calculations in Python/ex5.py
@@ -0,0 +1,6 @@
+a1 = 8
+a2 = 8 * 2
+n = 6
+q = a2 / a1
+s6 = a1 * ((1 - q**n) / (1 - q))
+print(f'The sum of the first {n} elements of the sequence is: {s6}')
