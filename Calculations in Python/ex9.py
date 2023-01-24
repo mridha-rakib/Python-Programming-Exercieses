60734ec7dd18f701c673c86248144e467a8dcb04
RAKIB <rakibmahmudmridha@gmail.com>
2023-01-24 09:16:38 +0600

    new file added

---
+++ Calculations in Python/ex9.py
@@ -0,0 +1,9 @@
+a = 1
+b = 5
+c = 4
+delta = b ** 2 - 4 * a * c
+delta_sqrt = delta ** (1/2)
+x1 = (-b - delta_sqrt) / (2 * a)
+x2 = (-b + delta_sqrt) / (2 * a)
+print(f'x1 = {x1}')
+print(f'x2 = {x2}')
