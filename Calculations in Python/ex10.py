138c2182213927f13b46800c6aadb04527e7198a
Rakib Mahmud Mridha <107380840+mridha-rakib@users.noreply.github.com>
2023-01-24 09:26:37 +0600

    "1 file added"

--- Calculations in Python/ex10.py
+++ Calculations in Python/ex10.py
@@ -1,3 +1,3 @@
-x1, x2, x3, x4 = 4, 3, 4.5, 5
-geo = (x1 * x2 * x3 * x4)**(1/4)
+a1, x2, x3, x4 = 4, 3, 4.5, 5
+geo = (a1 * x2 * x3 * x4)**(1/4)
 print(f'Geometric average of the given numbers: {geo:.2f}')
